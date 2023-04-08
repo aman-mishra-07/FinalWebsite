5a4d1132a6d25e2aee8950b33c03e3c31ab637c3
aman-mishra-07 <aman7986355740@gmail.com>
2023-04-08 09:40:36 +0530

    Table Updated

--- FinalWebsite/src/components/footer.jsx
+++ FinalWebsite/src/components/footer.jsx
@@ -1,5 +1,6 @@
 import { Footer } from "flowbite-react"
 import Logo from "../assets/logo.png"
+import { Link } from "react-router-dom"
 
 export default function Flowfooter() {
   return (
@@ -8,20 +9,20 @@ export default function Flowfooter() {
         <div className="w-full text-center">
           <div className="w-full justify-between sm:flex sm:items-center sm:justify-between text-white">
             <Footer.Brand
-              href="https://PVCstudio.com"
+              href="https://pvcstudio.github.io"
               name="PVC STUDIO"
             />
             <Footer.LinkGroup>
-              <Footer.Link href="#" className="pr-1">
-                About
+              <Footer.Link href="https://www.youtube.com/@amanpvcstudio4753" className="pr-1">
+                Youtube
               </Footer.Link>
-              <Footer.Link href="#" className="pr-1">
-                Privacy Policy
+              <Footer.Link href="https://www.facebook.com/" className="pr-1">
+               Facebook
               </Footer.Link>
-              <Footer.Link href="#" className="pr-1">
-                Licensing
+              <Footer.Link href="https://www.instagram.com/" className="pr-1">
+                Instagram
               </Footer.Link>
-              <Footer.Link href="#" className="pr-1">
+              <Footer.Link href="./Contact" className="pr-1">
                 Contact
               </Footer.Link>
             </Footer.LinkGroup>
